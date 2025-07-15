c12207658c3b8571ef5e015a18cbf4db007de20a
iamKimlong <iamKimlong@users.noreply.github.com>
2025-07-15 23:42:14 +0700

    config

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,6 +1,6 @@
 #---------------- Network Module ---------------------
 module "network" {
-  source                    = "git::https://github.com/AbdassalamAhmad/DevOps_Learning_Journey.git//Terraform/networking"
+  source                    = "git::https://github.com/iamKimlong/Deploying-AWS-webapp-with-Terraform-GitHub-Actions.git//Terraform/networking"
   vpc_cidr                  = var.vpc_cidr
   public_subnet_1_cidr      = var.public_subnet_1_cidr
   public_subnet_2_cidr      = var.public_subnet_2_cidr
